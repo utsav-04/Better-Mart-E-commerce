ef7d1bc719964e13b295555539471f046a32231b
utsav-04 <utsavraj.ptn04@gmail.com>
2022-07-17 23:07:54 +0530

    newcommit

--- Flipkart_clone/settings.py
+++ Flipkart_clone/settings.py
@@ -136,4 +136,4 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
